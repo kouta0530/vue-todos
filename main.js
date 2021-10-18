63247eb6196c4c57a9c3fe22eed22422dcd87d2f
kouta0530 <16jk047@ymklab.net>
2021-10-19 04:04:29 +0900

    ローカルストレージAPIを準備する

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ const app = new Vue({
   data: {},
   methods: {},
 });
+
+var STORAGE_KEY = "todos-vuejs-demo";
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.perse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
