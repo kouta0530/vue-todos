da0089bf9693c6c7ff05a0ab09b29a0000d504db
kouta0530 <16jk047@ymklab.net>
2021-10-19 23:06:40 +0900

    要素作成時にローカルストレージからデータを取得できないバグを修正

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var STORAGE_KEY = "todos-vuejs-demo";
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
+
 const app = new Vue({
   el: "#app",
   data: {
@@ -49,18 +64,3 @@ const app = new Vue({
     },
   },
 });
-
-var STORAGE_KEY = "todos-vuejs-demo";
-var todoStorage = {
-  fetch: function () {
-    var todos = JSON.perse(localStorage.getItem(STORAGE_KEY) || "[]");
-    todos.forEach(function (todo, index) {
-      todo.id = index;
-    });
-    todoStorage.uid = todos.length;
-    return todos;
-  },
-  save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-  },
-};
